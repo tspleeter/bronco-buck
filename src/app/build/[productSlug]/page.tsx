380fe13ff970ca3d61a45b5eb72c5ad41e7a587c
tspleeter <64556239+tspleeter@users.noreply.github.com>
2026-05-31 16:52:14 -0400

    Rename rear to back in view buttons

--- src/app/build/[productSlug]/page.tsx
+++ src/app/build/[productSlug]/page.tsx
@@ -303,7 +303,7 @@ export default function BuildPage() {
 
             {/* View Toggles */}
             <div style={{ display: "flex", gap: "8px", flexWrap: "wrap" }}>
-              {["front", "right", "rear", "angle"].map((v) => (
+              {["front", "right", "back", "angle"].map((v) => (
                 <button
                   key={v}
                   type="button"
