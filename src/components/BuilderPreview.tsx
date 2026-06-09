eda06fa3ebb952dc1d9bddd923b6beaa6105fba4
tspleeter <64556239+tspleeter@users.noreply.github.com>
2026-06-08 21:01:55 -0400

    Remove side view

--- src/components/BuilderPreview.tsx
+++ src/components/BuilderPreview.tsx
@@ -9,7 +9,7 @@ interface BuilderPreviewProps {
   mane?: ManeContext;
 }
 
-const BODY_VIEWS = ["front", "right", "side", "back", "left"];
+const BODY_VIEWS = ["front", "right", "back", "left"];
 
 export default function BuilderPreview({
   layers,
